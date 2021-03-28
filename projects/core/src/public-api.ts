2a9e08d150a6d6691b53ed675e8342aa1d1a9657
Chihab Otmani <chihab@gmail.com>
2021-03-28 15:42:39 +0100

    chore: build lib with ngc

--- projects/core/src/public-api.ts
+++ projects/core/src/public-api.ts
@@ -1,47 +1,5 @@
-import { ModuleWithProviders, NgModule, Provider } from '@angular/core';
-import { ACCESS_CONFIG } from './lib/config';
-import {
-  AccessStrategy,
-  FakeAccessStrategy,
-} from './lib/services/access-strategy.service';
-import { AccessDirective } from './lib/directives/access.directive';
-import { AccessExpressionDirective } from './lib/directives/access-expression.directive';
-import { AccessConfiguration } from './lib/helpers';
-
 export * from './lib/directives';
 export * from './lib/helpers';
 export * from './lib/services';
 export * from './lib/config';
-
-export interface AccessModuleConfig {
-  accesses?: AccessConfiguration;
-  redirect?: string;
-  strategy?: Provider;
-}
-
-@NgModule({
-  declarations: [AccessDirective, AccessExpressionDirective],
-  exports: [AccessDirective, AccessExpressionDirective],
-})
-export class AccessModule {
-  static forRoot(
-    config: AccessModuleConfig
-  ): ModuleWithProviders<AccessModule> {
-    return {
-      ngModule: AccessModule,
-      providers: [
-        {
-          provide: ACCESS_CONFIG,
-          useValue: {
-            accesses: config.accesses || {},
-            redirect: config.redirect || '/unauthorized',
-          },
-        },
-        config.strategy || {
-          provide: AccessStrategy,
-          useClass: FakeAccessStrategy,
-        },
-      ],
-    };
-  }
-}
+export * from './lib/core.module';
